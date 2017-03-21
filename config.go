2786615b2adcab813067f76092383a7c7302f9c5
Daniel M <danmondy@gmail.com>
2017-03-21 09:08:12 -0500

    db name added

--- config.go
+++ config.go
@@ -12,10 +12,11 @@ type Config struct {
 }
 
 type DbConnection struct {
-	Host     string "json:'host'"
-	Port     string "json:'port'"
-	Username string "json:'username'"
-	Password string "json:'password'"
+	Host     string `json:"host"`
+	Port     string `json:"port"`
+	Name     string `json:"name"`
+	Username string `json:"username"`
+	Password string `json:"password"`
 }
 
 func ReadConfig(file string, config interface{}) error {
